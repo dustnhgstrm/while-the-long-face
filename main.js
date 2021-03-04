e896885cc749dfd3b793ba74b5271cad436abccd
Dustin Hagstrom <dustin.l.hagstrom@gmail.com>
2021-03-04 14:08:07 -0500

    wrote functions to pass the tests.

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 function numberDoubler(num) {
-    // Your code here
+  let newNum = num;
+  while (newNum <= 100) {
+    newNum = newNum * 2;
+  }
+  return newNum;
 }
 
 function stringRepeater(str) {
-    // Your code here
+  let ourStr = str;
+  while (ourStr.length < 10) {
+    ourStr = ourStr + str;
+  }
+  return ourStr;
 }
 
 function makeDivisible(x, y) {
-    // Your code here
+  let ourX = x;
+  while (ourX % y !== 0) {
+    ourX++;
+  }
+  return ourX;
 }
 
 module.exports = {
-    numberDoubler,
-    stringRepeater,
-    makeDivisible
+  numberDoubler,
+  stringRepeater,
+  makeDivisible,
 };
